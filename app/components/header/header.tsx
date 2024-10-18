77d62b804598c0369ee68d47d42e06acdbb3ea45
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-18 20:14:27 -0300

    Update: Started header and button component added

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Header() {
   return (
-    <div className="w-full max-w-7xl mx-auto flex justify-between">
-      <div>aaa</div>
-      <div>aaa</div>
+    <div className="w-full max-w-7xl mx-auto flex justify-between p-5">
+      <div>UnplashBox</div>
+      <div className="flex gap-5">
+        <Button>Home</Button>
+        <Button>Collections</Button>
+      </div>
     </div>
   );
 }
