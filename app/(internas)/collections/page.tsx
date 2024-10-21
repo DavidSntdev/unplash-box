e38063aa3fa99fb2635822636497822f741f1bc6
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-21 15:58:51 -0300

    Update: Rename

--- app/(internas)/collections/page.tsx
+++ app/(internas)/collections/page.tsx
@@ -2,7 +2,7 @@ import Footer from "@/app/components/footer/footer";
 import Header from "@/app/components/header/header";
 import ConteudoCollection from "./components/conteudo";
 
-export default function Collections() {
+export default function CollectionsPage() {
   return (
     <div className="flex flex-col min-h-screen max-h-full justify-between">
       <div>
