3fc8a14efcdea55b516a7f7a4a3f3dce57454bc4
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-23 21:21:39 -0300

    Update: Create Button With Icon Component

---
+++ app/components/common/buttonWithIcon.tsx
@@ -0,0 +1,17 @@
+import { buttonStyle } from "@/app/utils/constants/buttonImage";
+import { ButtonInterface } from "@/app/utils/interfaces/button";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+interface ButtonWithIconProps extends ButtonInterface {
+  icon: string;
+}
+
+export default function ButtonWithIcon(props: ButtonWithIconProps) {
+  return (
+    <Button className={`${buttonStyle} flex gap-2`}>
+      <Image width={20} height={20} src={props.icon} alt={props.text} />
+      <span>{props.text}</span>
+    </Button>
+  );
+}
