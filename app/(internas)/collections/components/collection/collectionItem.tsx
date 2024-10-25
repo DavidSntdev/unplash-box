6ff589135f1c6e0852b034959cf9059f5c294f0c
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-25 19:14:16 -0300

    Update: Added Routes for Navigating to Selected Collections

--- app/(internas)/collections/components/collection/collectionItem.tsx
+++ app/(internas)/collections/components/collection/collectionItem.tsx
@@ -1,6 +1,7 @@
 import { getInfosCollection } from "@/app/utils/functions/getInfosCollections";
 import { unplashCollection } from "@/app/utils/interfaces/unplashCollection";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function CollectionItem({
   collection,
@@ -10,10 +11,16 @@ export default function CollectionItem({
   const { collectionId, titulo, imagens, quantidade, existeImagem } =
     getInfosCollection(collection);
 
+  const router = useRouter();
+  const handleImageClick = (id: string) => {
+    router.push(`/collections/${id}`);
+  };
+
   return (
     <div
       key={collectionId}
       className="flex flex-col gap-3 mx-auto cursor-pointer"
+      onClick={() => handleImageClick(titulo)}
     >
       {existeImagem && (
         <div className="w-[350px] h-[250px] overflow-hidden rounded-md shadow-md">
@@ -21,7 +28,7 @@ export default function CollectionItem({
             width={300}
             height={300}
             alt={titulo}
-            src={imagens[0]}
+            src={imagens[0].urls.full}
             className="object-cover w-full h-full"
           />
         </div>
