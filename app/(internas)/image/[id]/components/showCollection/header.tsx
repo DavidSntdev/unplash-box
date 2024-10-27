9c557d1d0f245d09983ad0a7c33288d1d151839c
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-26 22:28:05 -0300

    Update: Added Exit Button with Icon

---
+++ app/(internas)/image/[id]/components/showCollection/header.tsx
@@ -0,0 +1,23 @@
+import { buttonStyle } from "@/app/utils/constants/buttonImage";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+type props = {
+  setShowAddCollection: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function HeaderSAC({ setShowAddCollection }: props) {
+  return (
+    <div className="w-full flex justify-between items-center">
+      <h1 className="text-lg text-azulEscuro font-semibold">
+        Add to Collections
+      </h1>
+      <Button
+        className={`${buttonStyle} rounded-xl p-3`}
+        onClick={() => setShowAddCollection(false)}
+      >
+        <Image src="/icons/Close.svg" alt="close" width={20} height={20} />
+      </Button>
+    </div>
+  );
+}
