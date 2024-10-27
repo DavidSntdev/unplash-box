d6344f8ad67044a20207b1ed6b1842365a91575a
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-27 18:18:37 -0300

    Update: Added ClassName Prop Support

--- app/(internas)/collections/layouts/headerCollection.tsx
+++ app/(internas)/collections/layouts/headerCollection.tsx
@@ -3,18 +3,18 @@ import { textCollection } from "@/app/utils/constants/textCollection";
 interface props {
   text: string;
   description: string;
+  className?: string;
 }
 
 export default function HeaderCollections(props: props) {
   return (
-    <div className="flex flex-col mx-auto px-20 w-full">
+    <div className={`flex flex-col mx-auto w-full ${props.className}`}>
       <h1
-        className={`text-center text-4xl font-semibold py-2 px-4 rounded-md ${textCollection}`}
+        className={`text-center text-4xl font-semibold py-2 px-4 rounded-md ${textCollection} `}
         style={{ backgroundSize: "20% 20%" }}
       >
         {props.text}
       </h1>
-
       <p className="text-center max-w-md mx-auto">{props.description}</p>
     </div>
   );
