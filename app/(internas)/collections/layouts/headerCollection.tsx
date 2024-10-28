dffc3befe73b18f87d930002bcd11ba19ec81967
David Santos <162488821+DavidSntdev@users.noreply.github.com>
2024-10-28 20:54:56 -0300

    Update: Text Color Display Fix for Consistent Mobile and Desktop Views

--- app/(internas)/collections/layouts/headerCollection.tsx
+++ app/(internas)/collections/layouts/headerCollection.tsx
@@ -10,8 +10,8 @@ export default function HeaderCollections(props: props) {
   return (
     <div className={`flex flex-col mx-auto w-full ${props.className}`}>
       <h1
-        className={`text-center text-4xl font-semibold py-2 px-4 rounded-md ${textCollection} `}
-        style={{ backgroundSize: "20% 20%" }}
+        className={`text-center text-4xl font-semibold max-w-[300px] mx-auto py-2 px-4 rounded-md ${textCollection} `}
+        style={{ backgroundSize: "100% 100%" }}
       >
         {props.text}
       </h1>
